d0eea7d0d53628bf16f511ca5b4fd12cd6cbca5e
leaskagen <50326406+leaskagen@users.noreply.github.com>
2023-11-04 14:35:03 +0100

    Tab icon?

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { IconURL } from 'next/dist/lib/metadata/types/metadata-types'
+
+//const icon: IconURL = '/leaf-icon.png'
 
 export const metadata: Metadata = {
   title: 'Animal Crossing Villager Wiki',
   description: 'A wiki for all Animal Crossing villagers',
+  /*
   icons: {
-    icon: '/leaf-icon.png'
+    icon: icon,
   },
+  */
 }
 
 export default function RootLayout({
